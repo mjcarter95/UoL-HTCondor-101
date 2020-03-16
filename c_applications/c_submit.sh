eb41e5156912d616455fca39ca6a2eacca65fffc
Matthew Carter <m.j.carter2@liverpool.ac.uk>
2020-03-16 19:55:34 +0000

    c apps

--- c_applications/c_submit.sh
+++ c_applications/c_submit.sh
@@ -1,1 +1,13 @@
-c
+universe = vanilla
+executable = estimate_pi.exe
+arguments = $(PROCESS) 10000
+transfer_output_files = output$(PROCESS).txt
+should_transfer_files = YES
+when_to_transfer_output = ON_EXIT
+request_cpus = 1
+requirements = (Arch=="X86_64") && (OpSys=="WINDOWS")
+output = logs/stdout.log
+error = logs/stderr.log
+log = logs/log.log
+notification = never
+queue 10
