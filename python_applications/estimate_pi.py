04f89a454089c4570f2e7f0e8072da54444b70ff
Matthew Carter <m.j.carter2@liverpool.ac.uk>
2020-03-18 13:32:31 +0000

    finalising python example

--- python_applications/estimate_pi.py
+++ python_applications/estimate_pi.py
@@ -1,13 +1,13 @@
 import sys
 import numpy as np
 
-print(sys.argv)
-if len(sys.argv) < 2:
-    print("Usage {} <n_throws>".format(sys.argv[0]))
-    sys.exit()
+# if len(sys.argv) < 2:
+#     print("Usage {} <n_throws>".format(sys.argv[0]))
+#     sys.exit()
 
-n_throws = sys.argv[1]
-in_circle = 0
+# n_throws = sys.argv[1]
+n_throws = 10000
+in_circle = 0.
 
 # Throw the dart n_throws times
 x = np.random.uniform(0, 1, n_throws)
@@ -16,11 +16,11 @@ y = np.random.uniform(0, 1, n_throws)
 # Check if the dart hit the board
 for i in range(0, n_throws):
     if((x[i] * x[i]) + (y[i] * y[i]) <= 1):
-        in_circle += 1
+        in_circle += 1.
 
 # Estimate value of pi
-pi_estimate = 4 * in_circle / N
+pi_estimate = 4. * in_circle / n_throws
 
 # Write result to output file
 with open('output.txt', 'w') as f:
-    f.write(pi_estimate)
+    f.write('{}'.format(pi_estimate))
--- python_applications/python_submit.sh
+++ python_applications/python_submit.sh
@@ -1,1 +1,6 @@
-python
+python_script = estimate_pi.py
+indexed_output_files = output.txt
+stdout = logs/stdout.log
+stderr = logs/stderr.log
+log = logs/log.log
+total_jobs = 10
